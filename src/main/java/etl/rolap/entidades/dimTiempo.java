d71ff4eec3224b502647a361378f0d4504a1a0b5
g.delospinos <guille.pinos.ortiz@gmail.com>
2020-04-07 17:09:25 +0200

    gdelospi - scerrada - Hospital

---
+++ src/main/java/etl/rolap/entidades/dimTiempo.java
@@ -0,0 +1,112 @@
+package etl.rolap.entidades;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+public class dimTiempo {
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Id
+    private Long id;
+    private Date fecha;
+    private String dia;
+    private String mes;
+    private String anno;
+    private String cuatrim;
+    private String diaSemana;
+    private boolean esFinde;
+
+    public dimTiempo(Long id, Date fecha, String dia, String mes, String anno, String cuatrim, String diaSemana, boolean esFinde) {
+        this.id = id;
+        this.fecha = fecha;
+        this.dia = dia;
+        this.mes = mes;
+        this.anno = anno;
+        this.cuatrim = cuatrim;
+        this.diaSemana = diaSemana;
+        this.esFinde = esFinde;
+    }
+
+    public dimTiempo(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getAnno() {
+        return anno;
+    }
+
+    public void setAnno(String anno) {
+        this.anno = anno;
+    }
+
+    public String getCuatrim() {
+        return cuatrim;
+    }
+
+    public void setCuatrim(String cuatrim) {
+        this.cuatrim = cuatrim;
+    }
+
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public boolean isEsFinde() {
+        return esFinde;
+    }
+
+    public void setEsFinde(boolean esFinde) {
+        this.esFinde = esFinde;
+    }
+
+    @Override
+    public String toString() {
+        return "dimTiempo{" +
+                "id=" + id +
+                ", fecha=" + fecha +
+                ", dia='" + dia + '\'' +
+                ", mes='" + mes + '\'' +
+                ", anno='" + anno + '\'' +
+                ", cuatrim='" + cuatrim + '\'' +
+                ", diaSemana='" + diaSemana + '\'' +
+                ", esFinde=" + esFinde +
+                '}';
+    }
+}
