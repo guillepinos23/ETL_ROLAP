e4150539c13749a937a1ea0aa045a529b48c1270
g.delospinos <guille.pinos.ortiz@gmail.com>
2020-04-07 16:54:52 +0200

    gdelospi - scerrada - Hospital

---
+++ src/main/java/etl/rolap/entidades/dimHospital.java
@@ -0,0 +1,80 @@
+package etl.rolap.entidades;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+class dimHospital {
+
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    private String nombre;
+    private int codigoPostal;
+    private String autopista;
+    private String gestor;
+
+    public dimHospital(Long id, String nombre, int codigoPostal, String autopista, String gestor) {
+        this.id = id;
+        this.nombre = nombre;
+        this.codigoPostal = codigoPostal;
+        this.autopista = autopista;
+        this.gestor = gestor;
+    }
+
+    private dimHospital(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(int codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getAutopista() {
+        return autopista;
+    }
+
+    public void setAutopista(String autopista) {
+        this.autopista = autopista;
+    }
+
+    public String getGestor() {
+        return gestor;
+    }
+
+    public void setGestor(String gestor) {
+        this.gestor = gestor;
+    }
+
+    @Override
+    public String toString() {
+        return "dimHospital{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", codigoPostal=" + codigoPostal +
+                ", autopista='" + autopista + '\'' +
+                ", gestor='" + gestor + '\'' +
+                '}';
+    }
+}
--- src/main/java/etl/rolap/repositorios/RecursosRepository.java
+++ src/main/java/etl/rolap/repositorios/RecursosRepository.java
@@ -4,7 +4,6 @@ package etl.rolap.repositorios;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import etl.rolap.entidades.Recursos;
 @RepositoryRestResource
 public interface RecursosRepository extends CrudRepository<Recursos,Integer>{
 
